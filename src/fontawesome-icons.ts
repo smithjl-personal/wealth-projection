57236d31136e780a3478d1b7a11a24f459d8f113
master14270 <jessie.smith14270@gmail.com>
2025-11-13 19:16:36 -0500

    feat: Adds free font-awesome icons to the project.

---
+++ src/fontawesome-icons.ts
@@ -0,0 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlus, faTrash);
+
+export default FontAwesomeIcon;
